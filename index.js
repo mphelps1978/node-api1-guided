d765eec2f1a886083f1ff2faa7455af0e88948c0
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-02-10 13:04:24 -0600

    POST request complete

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express'); // common JS Modules
 const server = express();
 const port = 5000;
 
+server.use(express.json()) // needed for POST/PUT - It teaches Express to read JSON
+
 const hubs = require('./data/hubs-model')
 
 server.get('/', (req, res) => {
@@ -17,12 +19,33 @@ server.get ('/api/hubs', (req, res) => {
   // get the hubs from the database
   hubs.find()
   .then(hubs => {
+    // return a status of 200 to the user, and also return the requested info
     res.status(200).json(hubs)
   })
+  // handle any unknown errors
   .catch(err => {
-    console.log(err);
+    console.log(err)
     res.status(500).json({errorMessage: 'There was a problem'})
     })
 })
+
+// add a hub
+
+server.post('/api/hubs', (req, res) => {
+  // data to add will be in the body of the request
+  const hubinfo = req.body;
+  //validate the data and if valid, save it
+  hubs.add(hubinfo)
+  .then(hub => {
+    res.status(201).json(hub);
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({errorMessage: 'There was a problem'})
+    })
+})
+
+
+
 
 server.listen(port, () => console.log(`\n** Server is running on ${port}`));
